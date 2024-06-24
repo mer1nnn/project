472f80e49ccd67d9f5fb3a4b8db1b2c02edf46d6
mer1nnn <145487288+mer1nnn@users.noreply.github.com>
2024-06-25 00:12:42 +0300

    Update index.js

--- index.js
+++ index.js
@@ -9,6 +9,11 @@ function clearDisplay() {
 }
 
 function calculate() {
-    display.value = eval(display.value)
+    try {
+        display.value = eval(display.value)
+    }
+    catch(error) {
+        display.value = "Error"
+    }
 }
 
